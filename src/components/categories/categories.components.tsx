6754d25ae36679389d400b5890a7f3d52a2ecbef
Kleyton Ferreira <kleytondev40@gmail.com>
2026-03-21 19:11:39 -0300

    feat: add Categories Component

--- src/components/categories/categories.components.tsx
+++ src/components/categories/categories.components.tsx
@@ -1,17 +1,29 @@
+import { useEffect, useState } from 'react'
+
 // UTILITS
-import { useState } from 'react'
 import Category from '../../types/categories.types'
+import axios from 'axios'
+import env from '../../config/env.config'
 
 const Categories = () => {
   const [categories, setCategories] = useState<Category[]>([])
 
+  const fetchCategories = async () => {
+    try {
+      const { data } = await axios.get(`${env.apiUrl}/api/category`)
+      console.log({ data })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories()
+  }, [])
+
   return (
     <div className='categories-container'>
-      <div className='categories-content'>
-        {categories.map(() => (
-          <CategoriesItem />
-        ))}
-      </div>
+      <div className='categories-content'>Aqui vem as Categorias ( ! )</div>
     </div>
   )
 }
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/header/header.components'
+import Categories from '../components/categories/categories.components'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Categories />
     </>
   )
 }
