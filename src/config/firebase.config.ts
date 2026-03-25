3dd324c1d629325e827e831a08c6f8107511f8a4
Kleyton Ferreira <kleytondev40@gmail.com>
2026-03-25 17:53:34 -0300

    feat: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCY500m5M54XTK7ZcaBKoTTZC8ffe6ybV0",
+    authDomain: "ecommerce-my-56ea8.firebaseapp.com",
+    projectId: "ecommerce-my-56ea8",
+    storageBucket: "ecommerce-my-56ea8.firebasestorage.app",
+    messagingSenderId: "665394247766",
+    appId: "1:665394247766:web:f7d72f33eb1b70576a23f7"
+};
+
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
