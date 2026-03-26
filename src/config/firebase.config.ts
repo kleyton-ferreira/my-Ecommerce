4f6e179c7f6fc648eb3e6a16e68a9d6c7b749fe4
Kleyton Ferreira <kleytondev40@gmail.com>
2026-03-26 17:23:13 -0300

    feat: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,6 +1,7 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
 
 
 const firebaseConfig = {
@@ -15,3 +16,5 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
+export const auth = getAuth(app)
+
