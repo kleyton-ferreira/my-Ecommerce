fe745fbb97a20c5097e96fa2da9aaa92990c3f99
Kleyton Ferreira <kleytondev40@gmail.com>
2026-03-26 17:23:45 -0300

    feat: add user account creation

--- src/pages/sign-up/sign-up-page.tsx
+++ src/pages/sign-up/sign-up-page.tsx
@@ -1,5 +1,8 @@
 import { FiLogIn } from 'react-icons/fi'
 import validator from 'validator'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { auth, db } from '../../config/firebase.config'
+import { addDoc, collection } from 'firebase/firestore'
 
 // COMPONENTS
 import { useForm } from 'react-hook-form'
@@ -17,7 +20,7 @@ import {
 } from './sign-up-style'
 
 interface SignUpForm {
-  name: string
+  firstName: string
   lastName: string
   email: string
   password: string
@@ -34,8 +37,23 @@ const SignUpPage = () => {
 
   const watchPassword = watch('password')
 
-  const handleSubmitSignUpPage = (data: SignUpForm) => {
-    console.log({ data })
+  const handleSubmitSignUpPage = async (data: SignUpForm) => {
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      )
+      console.log({ userCredentials })
+      await addDoc(collection(db, 'users'), {
+        id: userCredentials.user.uid,
+        email: userCredentials.user.email,
+        firstName: data.firstName,
+        lastName: data.lastName
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   console.log({ errors })
@@ -50,11 +68,11 @@ const SignUpPage = () => {
           <SigUpInputContainer>
             <p>Nome</p>
             <CustomInput
-              hasError={!!errors.name}
+              hasError={!!errors.firstName}
               placeholder='Digite seu nome'
-              {...register('name', { required: true })}
+              {...register('firstName', { required: true })}
             />
-            {errors?.name?.type === 'required' && (
+            {errors?.firstName?.type === 'required' && (
               <InputErrorMessage>O nome é obrigatório.</InputErrorMessage>
             )}
           </SigUpInputContainer>
