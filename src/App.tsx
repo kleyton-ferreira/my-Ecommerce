af418dcf4e5fcc7f77838718c40f64f79ffc5e38
Kleyton Ferreira <kleytondev40@gmail.com>
2026-03-27 19:33:11 -0300

    feat: add auth state listener and sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, use } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// UTILITZ
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './config/firebase.config'
 
 // PAGES
 import Home from './pages/home/home.page'
 import LoginPage from './pages/login/login.page'
 import SignUpPage from './pages/sign-up/sign-up-page'
 
-interface AppProps {
-  children?: string
-}
+const App: FunctionComponent = () => {
+  onAuthStateChanged(auth, (user) => {
+    console.log(user)
+  })
 
-const App: FunctionComponent<AppProps> = ({ children }) => {
   return (
     <>
       <BrowserRouter>
--- src/components/header/header.components.tsx
+++ src/components/header/header.components.tsx
@@ -1,5 +1,9 @@
 import { useNavigate } from 'react-router-dom'
 
+// AQUI E PRA FUNÇAO DE TESTE SAIR
+import { auth } from '../../config/firebase.config'
+import { signOut } from 'firebase/auth'
+
 // ICONS
 import { BsCart3 } from 'react-icons/bs'
 
@@ -36,6 +40,7 @@ const Header = () => {
         <HeaderItems>Explorar</HeaderItems>
         <HeaderItems onClick={handleLoginClick}>Login</HeaderItems>
         <HeaderItems onClick={handleSignUpClick}>Criar Conta</HeaderItems>
+        <HeaderItems onClick={() => signOut(auth)}>Sair</HeaderItems>
         <HeaderCart>
           <BsCart3 size={20} />
           <HeaderParagraf>5</HeaderParagraf>
